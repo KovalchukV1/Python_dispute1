61a2c558f13f39fbd6b574ebdd261df52d091fa0
KovalchukV1 <kovalchukvv001@gmail.com>
2022-08-18 16:53:41 +0300

    upgrade

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
+
+from abstract.selenium_listener import MyListener
 
 
 @pytest.fixture
 def get_chrome_options():
     options = chrome_options()
-    options.add_argument('chrome')
+    options.add_argument('chrome')  # Use headless if you do not need a browser UI
     options.add_argument('--start-maximized')
     options.add_argument('--window-size=1650,900')
     return options
@@ -17,17 +20,15 @@ def get_webdriver(get_chrome_options):
     options = get_chrome_options
     driver = webdriver.Chrome(options=options)
     return driver
-# usr -> local -> bin
 
 
 @pytest.fixture(scope='function')
 def setup(request, get_webdriver):
     driver = get_webdriver
+    driver = EventFiringWebDriver(driver, MyListener())
     url = 'https://www.macys.com/'
     if request.cls is not None:
         request.cls.driver = driver
     driver.get(url)
     yield driver
     driver.quit()
-
-
---
+++ tests/test_homepageall.py
@@ -0,0 +1,13 @@
+import pytest
+
+from pom.homepage_nav import HomepageNav
+
+
+@pytest.mark.usefixtures('setup')
+class TestHomepage:
+
+    def test_all(self):
+        self
+
+
+
